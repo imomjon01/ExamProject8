9e60df3c63d85386b8d8f5b21d5175aad698fd6d
macbook.u <imomjonuzb@gmail.com>
2025-04-28 16:07:12 +0500

    commit

--- src/main/java/uz/pdp/exam/entity/User.java
+++ src/main/java/uz/pdp/exam/entity/User.java
@@ -1,6 +1,11 @@
 package uz.pdp.exam.entity;
 
 public class User {
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
 
 
 }
